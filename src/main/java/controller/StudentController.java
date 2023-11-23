08fff6947b8588151f6c1313aeded3b3b92fd8c7
chandra-98a <chandrakalaa2022@gmail.com>
2023-11-23 15:06:05 +0530

    "created Student controller"

---
+++ src/main/java/controller/StudentController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import bean.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    @GetMapping("/student")
+    public Student getStudent(){
+        int id;
+        Student student= new Student(1,
+                "chandra", "kala"
+        );
+        return student;
+    }
+}
